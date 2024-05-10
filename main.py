a0882d2eda432217a67d4fbe54729f745bf35e0c
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2024-05-10 12:02:55 +0300

    connect with sheet

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import gspread
 from google.oauth2.service_account import Credentials
+
+
+scopes = [
+    'https://www.googleapis.com/auth/spreadsheets'
+]
+
+creds = Credentials.from_service_account_file('credentials.json', scopes=scopes)
+client = gspread.authorize(creds)
+
+sheet_id = '1V9cRH7Oq-rF0pa2ADtHAKNvOImAMzTwt-vRRt7tvn1U'
+sheet = client.open_by_key(sheet_id)
