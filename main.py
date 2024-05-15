c86164bda5e2b5d215eca274b33ad720d5229362
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2024-05-15 20:25:37 +0300

    populate sheet with all videos by status and by editor respectivelly

--- main.py
+++ main.py
@@ -34,10 +34,11 @@ def create_worksheets():
 
 curr_worksheet = sheet.worksheet(f'{curr_month} {curr_year}')
 # data = scrape_data()
-data = {'Hani Sinno': {'review/re-edit': {'num': 3, 'videos': "Giving Horrible Advice To Redditors, Reacting to My Old Tiktok's | Cringe, Drunk Drag!, "}, 'total_videos': {'num': 10}, 'Ongoing edit': {'num': 4, 'videos': 'READ BRIEF_These Amazon Products Should Be ILLEGAL, Reacting To And Making Overpriced Art, The Trad Wife Epidemic Is Insane, RE-EDIT : Entitled Karens That Were Called Out On Social Media - REACTION, '}, 'Ready to edit': {'num': 3, 'videos': 'Snap from FB - These Amazon Products Should Be ILLEGAL, Snap from FB - Reacting To And Making Overpriced Art, Snap from FB - The Trad Wife Epidemic Is Insane, '}, 'AB Test': {'num': 0}}, 
-               'Patriot Bytyqi': {'Ready to edit': {'num': 3, 'videos': 'petty divorce drama that made it to AITA - REACTION, FB to Snap - Bad honeymoons that got EXPOSED on TikTok - REACTION, FB to Snap - Watching TikToks until I find the world’s craziest Ex - REACTION, '}, 'total_videos': {'num': 9}, 'Ongoing edit': {'num': 4, 'videos': 'Bad honeymoons that got EXPOSED on TikTok - REACTION, Watching TikToks until I find the world’s craziest Ex - REACTION, FB to Snap - Part 1 - petty beef that turned SAVAGE - REACTION, FB to Snap - Part 2 - petty beef that turned SAVAGE - REACTION, '}, 'review/re-edit': {'num': 1, 'videos': 'petty beef that turned SAVAGE - REACTION, '}, 'AB Test': {'num': 1, 'videos': 'Vertical - from BFF to BACKSTABBER - REACTION, '}}, 
-               'Betim Mehani': {'Ongoing edit': {'num': 4, 'videos': "Reacting To Your INSANE Wedding Stories - REACTION, What You Didn’t Know About America's Most INFAMOUS Serial Killer, call the whole wedding off right now! - REACTION, This Show Needs To Be Cancelled…, "}, 'total_videos': {'num': 11}, 'review/re-edit': {'num': 1, 'videos': 'Cheaters ruining their marriages in 10 seconds or less - REACor less - REACTION, '}, 'Ready to edit': {'num': 2, 'videos': 'Snap from FB - This Show Needs To Be Cancelled…, FB to Snap - call the whole wedding off right now! - REACTION, '}, 'AB Test': {'num': 4, 'videos': "married couple drama that lit up AITA - REACTION, TIKtok Is Rotting My Brain…, LOCKED INSIDE AMERICA'S MOST HAUNTED ASYLUM ft @CelinaSpookyBoo | PART ONE, WE DID A SEANCE IN THE MOST HAUNTED CITY IN AMERICA Ft @CelinaSpookyBoo | PART 1, "}}, 
-               'Stoyan Kolev': {'Ready to edit': {'num': 1, 'videos': 'Craziest People That OBJECTED At Weddings - REACTION, '}, 'total_videos': {'num': 5}, 'review/re-edit': {'num': 3, 'videos': 'Part 1 -\xa0FB to Snap\xa0- serving up heaping slices of humble pie - REACTION, Part 2 -\xa0FB to Snap\xa0- serving up heaping slices of humble pie - REACTION, Part 3 - FB to Snap - serving up heaping slices of humble pie - REACTION, '}, 'AB Test': {'num': 1, 'videos': 'serving up heaping slices of humble pie - REACTION, '}, 'Ongoing edit': {'num': 0}}}
+data = {'Hani Sinno': {'Ready to edit': {'num': 6, 'videos': ['Snap from FB - These Amazon Products Should Be ILLEGAL', 'Snap from FB - Reacting To And Making Overpriced Art', 'Snap from FB - The Trad Wife Epidemic Is Insane', 'I Bought DISCONTINUED Products From Our Childhood', 'Snap from FB - Part 1 - I Bought DISCONTINUED Products From Our Childhood', 'Snap from FB - Part 2 - I Bought DISCONTINUED Products From Our Childhood']}, 'Ongoing edit': {'num': 2, 'videos': ['Driving Across America', 'Reacting To And Making Overpriced Art']}, 'review/re-edit': {'num': 2, 'videos': ['Giving Horrible Advice To Redditors', 'RE-EDIT : Entitled Karens That Were Called Out On Social Media - REACTION']}, 'AB Test': {'num': 5, 'videos': ['AB to relaunch_New Edit - Tiktok’s PSYCHO Ex Girlfriends', 'AB to relaunch_Couples Content Makes Me Cringe', "Reacting to My Old Tiktok's | Cringe", 'Drunk Drag!', 'The Trad Wife Epidemic Is Insane']}, 'Delivered': {'num': 0, 'videos': []}, 'total_videos': {'num': 15}},
+        'Patriot Bytyqi': {'Ready to edit': {'num': 4, 'videos': ['petty divorce drama that made it to AITA - REACTION', 'confessions that cancelled the wedding - REACTION', 'Snap - 5 Minute Crafts that Make My Brain Hurt', 'Snap from FB - influencers that thought they were the main character - REACTION']}, 'Ongoing edit': {'num': 2, 'videos': ['FB to Snap - Bad honeymoons that got EXPOSED on TikTok - REACTION', 'FB to Snap - Watching TikToks until I find the world’s craziest Ex - REACTION']}, 'review/re-edit': {'num': 1, 'videos': ['influencers that thought they were the main character - REACTION']}, 'AB Test': {'num': 2, 'videos': ['Bad honeymoons that got EXPOSED on TikTok - REACTION', 'Watching TikToks until I find the world’s craziest Ex - REACTION']}, 'Delivered': {'num': 0, 'videos': []}, 'total_videos': {'num': 9}}, 
+        'Betim Mehani': {'Ready to edit': {'num': 3, 'videos': ['Dumb People That Need To Get Off Social Media - REACTION', 'RE-EDIT : Entitled Bridezillas And Their Insane Demands - REACTION', "TikTok's that Need to Stay in 2021"]}, 'Ongoing edit': {'num': 3, 'videos': ["What You Didn’t Know About America's Most INFAMOUS Serial Killer", 'Snap from FB - This Show Needs To Be Cancelled…', 'FB to Snap - call the whole wedding off right now! - REACTION']}, 'review/re-edit': {'num': 3, 'videos': ['Reacting To Your INSANE Wedding Stories - REACTION', 'call the whole wedding off right now! - REACTION', 'This Show Needs To Be Cancelled…']}, 'AB Test': {'num': 3, 'videos': ['TIKtok Is Rotting My Brain…', "LOCKED INSIDE AMERICA'S MOST HAUNTED ASYLUM ft @CelinaSpookyBoo | PART ONE", 'WE DID A SEANCE IN THE MOST HAUNTED CITY IN AMERICA Ft @CelinaSpookyBoo | PART 1']}, 'Delivered': {'num': 0, 'videos': []}, 'total_videos': {'num': 12}}, 
+        'Stoyan Kolev': {'Ready to edit': {'num': 1, 'videos': ['what did I just read?! judging your AITA stories']}, 'Ongoing edit': {'num': 0, 'videos': []}, 'review/re-edit': {'num': 0, 'videos': []}, 'AB Test': {'num': 1, 'videos': ['Craziest People That OBJECTED At Weddings - REACTION']}, 'Delivered': {'num': 0, 'videos': []}, 'total_videos': {'num': 2}}
+        }
 
 def update_tracker():
     tracker_worksheet = sheet.worksheet('Tracker')
@@ -57,23 +58,25 @@ def update_tracker():
 
     tracker_worksheet.update_cells(cells)
 
-def display_total_videos():
-    row = 1
-    header_cells = curr_worksheet.range('A1:L1')
-    headers_data = []
 
-    for editor, column in EDITORS.items():
-        headers_data.append(f'{editor} total videos')
-        headers_data.append(data[editor]['total_videos']['num'])
-        headers_data.append('') # leaves an empty cell inbetween editors
+def add_videos_to_sheet():
+    column = 65 # ascii for first column A
+    for editor in data.keys():
+        for status in data[editor]:
+            if status == 'total_videos':
+                continue
+            num_videos = data[editor][status]['num']
+            if num_videos == 0:
+                column += 1
+                continue
+            videos = data[editor][status]['videos']
+            column_cells = curr_worksheet.range(f'{chr(column)}3:{chr(column)}{num_videos + 2}')
+            index = 0
+            for cell in column_cells:
+                cell.value = videos[index]
+                index += 1
+            curr_worksheet.update_cells(column_cells)
+            column += 1
 
-    index = 0
-    for cell in header_cells:
-        cell.value = headers_data[index]
-        index += 1
 
-    curr_worksheet.update_cells(header_cells)
-
-
-update_tracker()
-# display_total_videos()
+add_videos_to_sheet()
